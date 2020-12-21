b23d9f07a3b289b428d38152080b4083536f5cf6
Tim La <email@example.com>
2020-12-20 23:04:12 -0500

    changed homescreen title

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,12 @@ const App = () => {
     <div className='homepage_container'>
       <div className='navbar_container'></div>
       <Row className='homepage_section'>
-        <Col className='homepage_column column_left'>
-          <h1>
+        <Col className='homepage_column column_left' md={5}>
+          {/* <h1>
             How Can We Work <span>Together</span> to Accomplish Our Goals
-          </h1>
+          </h1> */}
+          <h1>"83% of the population does not have goals"</h1>
+          <cite>-Harvard Business Study</cite>
         </Col>
         <Col className='homepage_column column_right'>
           <div className='homepage_image'></div>
