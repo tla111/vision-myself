3c2de0ff094925a3bfbc75db58a3f65afa516000
Tim La <email@example.com>
2020-12-23 18:34:28 -0500

    implemented carousel to display goals

--- client/src/components/vision/DisplayGoals.js
+++ client/src/components/vision/DisplayGoals.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { CircularProgress } from '@material-ui/core';
+import Carousel from 'react-elastic-carousel';
 import './posts.css';
 
 const DisplayGoals = () => {
   const posts = useSelector((state) => state.postsReducer);
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
   return !posts.length ? (
     <CircularProgress />
   ) : (
     <div className='posts_container'>
-      {posts.map((post) => (
-        <div key={post._id}>
-          <div className='card'>
-            <img src={post.selectedFile} alt='image_goal' />
-            <div className='card_body'>
-              <h2>{post.title}</h2>
-              <p>{post.message}</p>
-              <h5>Posted By: {post.author}</h5>
+      <Carousel breakPoints={breakPoints}>
+        {posts.map((post) => (
+          <div key={post._id}>
+            <div className='card'>
+              <img src={post.selectedFile} alt='image_goal' />
+              <div className='card_body'>
+                <h2>{post.title}</h2>
+                <p>{post.message}</p>
+                <h5>Posted By: {post.author}</h5>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </Carousel>
     </div>
   );
 };
